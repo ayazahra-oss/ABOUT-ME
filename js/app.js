54ceca7b358f7b62a35b1552e27967cbbf66ad90
ayazahra-oss <aya.fawaz.za@gmail.com>
2021-06-07 22:49:08 +0300

    fiel

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("aya");
+
+let userName = prompt("Whats your name ?");
+alert("welcome to our websit " + userName );
+
+console.log(userName);
+var userAge= prompt("what your old ?");
+alert("welcom to our websit"+ userAge);
+
+var catName= prompt("what your catName?");
+alert("welcom to your cat"+ catName);
+
+var yourDate= prompt("what is the Date Today?")
+alert("Welcom any time"+yourDate);
