c4dbc41ef055e7c51f5298c524e2630ebfcdd8d3
ayazahra-oss <aya.fawaz.za@gmail.com>
2021-06-07 23:26:44 +0300

    file

--- js/app.js
+++ js/app.js
@@ -12,3 +12,13 @@ alert("welcom to your cat"+ catName);
 
 var yourDate= prompt("what is the Date Today?")
 alert("Welcom any time"+yourDate);
+
+
+
+if(makeTrouble === 'yes' || makeTrouble === 'y') {
+    alert('Calm down ');
+  } else if (makeTrouble === 'no' || makeTrouble === 'n') {
+    alert('Good job');
+  } else {
+    alert('Pease type (yes) or (no) or (y) or (n)');
+  }
