bc7366c9c5f10bfd0d66f6649e9dffc7c0de1e2f
ayazahra-oss <aya.fawaz.za@gmail.com>
2021-06-08 03:05:42 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-console.log("aya");
+'use strict';
 
 let userName = prompt("Whats your name ?");
 alert("welcome to our websit " + userName );
 
 console.log(userName);
-var userAge= prompt("what your old ?");
+let userAge= prompt("what your old ?").toLowerCase();
+if(userAge ==='21'|| userAge === '21')
 alert("welcom to our websit"+ userAge);
 
 var catName= prompt("what your catName?");
@@ -13,12 +14,25 @@ alert("welcom to your cat"+ catName);
 var yourDate= prompt("what is the Date Today?")
 alert("Welcom any time"+yourDate);
 
+let cat =prompt("if i love A cat?").toLowerCase();
+if(cat=== "yes" || cat=== "y"){
+    alert("You Have A correct Answer");
+}
+else if (cat=="no"||cat =="n") {
+    alert("You Have Uncorrect Answer");
+}
+else{
+    alert("Please Enter Yes , No ,Y,N");
+}
+
+let coffee =prompt("if I love A coffee ?").toLowerCase();
+if(coffee=="yes" || coffee=="y"){
+    alert("you are right");
+}
+else if (coffee=="no" || coffee=="n"){
+    alert("you are not right")
+}else{
+    alert("enter please yes or no or y or n ");
+}
 
 
-if(makeTrouble === 'yes' || makeTrouble === 'y') {
-    alert('Calm down ');
-  } else if (makeTrouble === 'no' || makeTrouble === 'n') {
-    alert('Good job');
-  } else {
-    alert('Pease type (yes) or (no) or (y) or (n)');
-  }
